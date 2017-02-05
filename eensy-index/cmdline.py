01bc34df13e54d2b3367005e6d6ac52747f7ae2c
Jason Orendorff <jason.orendorff@gmail.com>
2017-02-05 11:54:59 -0600

    Remove eensy.

--- eensy-index/cmdline.py
+++
@@ -1,7 +0,0 @@
-import eensy
-
-my_index = eensy.Index("../eensy-sample")
-while True:
-    search_query = input("?> ")
-    for doc, score in my_index.search(search_query, 10):
-        print("{:7.1f} {}".format(score, doc.name))
